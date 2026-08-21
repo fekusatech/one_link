dc1e9b54ae46aee96164a504bd1a280474f0888d
fekusadev <santosofebrikukuh.dev@gmail.com>
2026-08-21 09:29:18 +0700

    fix(profile): add missing http and shared_preferences imports in profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import '../config/app_config.dart';
 import '../constants/app_colors.dart';
 import '../constants/app_text_styles.dart';
